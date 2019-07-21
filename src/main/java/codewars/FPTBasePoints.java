0f340f82a51428523134c7f0ad5950382bb488f0
stas <q>
2019-07-21 21:37:31 +0300

    four path transport, base points algorithm

--- src/main/java/codewars/FPTBasePoints.java
+++ src/main/java/codewars/FPTBasePoints.java
@@ -1,9 +1,8 @@
 package codewars;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class FPT {
+public class FPTBasePoints {
 
     /*
     The eccentric candy-maker, Billy Bonka, is building a new candy factory to produce his new 4-flavor sugar pops. The candy is made by placing a piece of candy base onto a conveyer belt which transports the candy through four separate processing stations in sequential order. Each station adds another layer of flavor.
@@ -34,7 +33,7 @@ Test Example
 
 // INPUT - reference image A
 int[] stations = {0,65,93,36};
-new FPT(stations).solve();
+new FPTPermute(stations).solve();
 
 // OUTPUT #1 - reference image B
 // [0, 1, 2, 3, 4, 5, 15, 25, 35, 45, 55, 65, 64, 63, 73, 83, 93, 94, 95, 96, 86, 76, 66, 56, 46, 36]
@@ -47,65 +46,115 @@ new FPT(stations).solve();
 
 	int n = 10;
 	int[] stations;
-	int[] path;
-	Set<Integer> usedSquares = new HashSet<>();
-//    int squareFrom, squareTo;
+	Set<Integer> usedNodesForPreviousPaths = new HashSet<>();
 
-	public FPT(int[] stations) {
+	public FPTBasePoints(int[] stations) {
 		this.stations = stations;
-//        squareFrom = path[0];
-//        squareTo = path[1];
 	}
-
-
-	public List<Integer> solve() {
-//		List<List<Node>> basePoints = findBasePoints();
-		int ind = 0;
-		return getMinPathLength(ind);
-//		return Arrays.stream(path).boxed().collect(Collectors.toList());
-	}
-
-//	List<List<Node>> findBasePoints(Node from, Node to) {
-//		Node node1 = new Node(from.x, to.y);
-//
-//		Node node2 = new Node(to.x, from.y);
-//
-//		int nodeX, nodeY;
-//		Node over = new Node(stations[2]);
-//		Node leftBottom = new Node(Math.min(from.x, to.x), Math.min(from.y, to.y));
-//		Node rightTop = new Node(Math.max(from.x, to.x), Math.max(from.y, to.y));
-//		if (over.x <= leftBottom.x) {
-//			nodeX = over.x - 1;
-//		} else if (over.x >= rightTop.x) {
-//			nodeX = over.x + 1;
-//		} else {
-//			nodeX = over.x;
-//		}
-//		if (over.y <= leftBottom.y) {
-//			nodeY = over.y - 1;
-//		} else if (over.y >= rightTop.y) {
-//			nodeY = over.y + 1;
-//		} else {
-//			nodeY = over.y;
-//		}
-//		Node node3 = new Node(nodeX, nodeY);
-//
-//	}
 
 	public static void main(String[] args) {
-		int[] stations = {0, 65, 93, 36};
-		System.out.println(Arrays.toString(new FPT(stations).solve().toArray()));
+//		{
+//			int[] stations = {0, 65, 93, 36};
+//			System.out.println(Arrays.toString(new FPTBasePoints(stations).solve().toArray()));
+//		}
+//		{
+//			int[] stations = {0, 95, 93, 36};
+//			System.out.println(Arrays.toString(new FPTBasePoints(stations).solve().toArray()));
+//		}
+		{
+			int[] stations = {16, 10, 18, 14};
+			System.out.println(Arrays.toString(new FPTBasePoints(stations).solve().toArray()));
+		}
 	}
 
-	private List<Integer> getMinPathLength(int ind) {
-		List<Integer> res = new LinkedList<>();
-		res.addAll(findPath(stations[0], stations[1]));
-		res.addAll(findPath(stations[1], stations[2]));
-		res.addAll(findPath(stations[2], stations[3]));
-		res.add(stations[3]);
-		return res;
+	public List<Integer> solve() {
+		return getMinPathLength();
 	}
 
+	private Node findNextBasePoint(Node from, Node to, Node over) {
+		int nodeX, nodeY;
+		Node leftBottom = new Node(Math.min(from.x, to.x), Math.min(from.y, to.y));
+		Node rightTop = new Node(Math.max(from.x, to.x), Math.max(from.y, to.y));
+		if (over.x <= leftBottom.x) {
+			nodeX = over.x - 1;
+		} else if (over.x >= rightTop.x) {
+			nodeX = over.x + 1;
+		} else {
+			nodeX = over.x;
+		}
+		if (over.y <= leftBottom.y) {
+			nodeY = over.y - 1;
+		} else if (over.y >= rightTop.y) {
+			nodeY = over.y + 1;
+		} else {
+			nodeY = over.y;
+		}
+		return new Node(nodeX, nodeY);
+	}
+
+
+	private List<Integer> getMinPathLength() {
+		List<Integer> allPath = null;
+		int minLen = Integer.MAX_VALUE;
+
+		int currStationInd = 0;
+		int currentPoint = stations[currStationInd];
+		int nextPoint = stations[currStationInd + 1];
+		for (int st : stations) {
+//			if (st) {
+//				Node basePoint = findNextBasePoint(new Node(currentPoint), new Node(nextPoint), )
+//			}
+		}
+//
+//		while (true) {
+//
+//		}
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (j == i) continue;
+				for (int k = 0; k < 3; k++) {
+					if (k == i || k == j) continue;
+					System.out.println();
+					List<Integer> res = new LinkedList<>();
+					usedNodesForPreviousPaths = new HashSet<>();
+					Object[] paths = new Object[3];
+
+					paths[i] = calcPath(i);
+					if (paths[i] == null) continue;
+
+					paths[j] = calcPath(j);
+					if (paths[j] == null) continue;
+
+					paths[k] = calcPath(k);
+					if (paths[k] == null) continue;
+
+					for (Object path : paths) {
+						res.addAll((List<Integer>) path);
+					}
+					res.add(stations[3]);
+
+					if (res.size() < minLen) {
+						minLen = res.size();
+						allPath = res;
+					}
+				}
+			}
+		}
+		return allPath;
+	}
+
+	private List<Integer> calcPath(int i) {
+		for (int st : stations) {
+			if (st == stations[i] || st == stations[i + 1]) {
+				usedNodesForPreviousPaths.remove(st);
+			} else {
+				usedNodesForPreviousPaths.add(st);
+			}
+		}
+		List<Integer> path = findPath(stations[i], stations[i + 1]);
+		return path;
+	}
 
 	private List<Integer> findPath(int from, int to) {
 		// fill length matrix
@@ -134,7 +183,6 @@ new FPT(stations).solve();
 			int min = Integer.MAX_VALUE;
 			Node node = null;
 			for (int i = 0; i < n * n; i++) {
-//				node = new Node(i);
 				if (!usedNodes.contains(new Node(i)) && minLength[i] != null && minLength[i] < min) {
 					min = minLength[i];
 					node = new Node(i);
@@ -143,6 +191,7 @@ new FPT(stations).solve();
 			if (node == null) break;
 			usedNodes.add(node);
 
+			if (distanceMap.get(node) == null) return null;
 			for (Node nodeTo : distanceMap.get(node)) {
 				if (!usedNodes.contains(nodeTo)) {
 					if (minLength[nodeTo.position] == null) {
@@ -156,30 +205,30 @@ new FPT(stations).solve();
 		}
 		System.out.println("dist to " + to + "=" + minLength[to]);
 
+		// no path
+		if (minLength[to] == null) return null;
+
+		// gather result
 		List<Integer> res = new ArrayList<>();
 		int nextInd = to;
-//		res.add(nextInd);
 		while (true) {
 			nextInd = prevVertex[nextInd].position;
-			res.add(0,nextInd);
+			res.add(0, nextInd);
+			if (nextInd != from && nextInd != to)
+				usedNodesForPreviousPaths.add(nextInd);
 			if (nextInd == from) break;
 		}
+		System.out.println("path for " + from + "-" + to + ": " + Arrays.toString(res.toArray()));
 		return res;
 	}
 
+	// fill distance map
 	private void addMove(Map<Node, Set<Node>> moveMap, Node from, Node to) {
+		if (usedNodesForPreviousPaths.contains(from.position) || usedNodesForPreviousPaths.contains(to.position))
+			return;
 		Set<Node> list = moveMap.computeIfAbsent(from, k -> new HashSet<>());
 		list.add(to);
 	}
-
-	private int getMin(int... numbers) {
-		int min = 0;
-		for (int n : numbers) {
-			min = Math.min(min, n);
-		}
-		return min;
-	}
-
 
 	class Node implements Comparable<Node> {
 		final Integer position;
